413fb0a68121c72a36102e98bf5df46cd5773a3b
Alexander Denk <mail@alexander-denk.net>
2016-11-05 12:41:04 +0100

    PBL Library - Fixed String Comparisons for IronPython

---
+++ FMSuite/External/CNFGenerator.py
@@ -0,0 +1,13 @@
+﻿#! /usr/bin/env python2.7
+
+import sys
+import PyBool_public_interface as Bool
+
+expression = Bool.parse_std(expressionFileInput)
+expression = expression["main_expr"]
+expression = Bool.simplify(expression)
+expression = Bool.exp_cnf(expression)
+result = Bool.print_expr(expression)
+output = open(expressionFileOutput, 'w')
+output.write(result)
+output.close()
